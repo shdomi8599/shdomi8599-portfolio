e418890e2729ee11e87c3e0ee991ee6c7bcc43a2
shdomi8599 <shdomi@naver.com>
2023-03-31 00:21:19 +0900

    chore : useInput 타입 수정

--- function/hooks/useInput.tsx
+++ function/hooks/useInput.tsx
@@ -1,25 +1,28 @@
-// useState와 useCallback을 react에서 import합니다.
-import { useState, useCallback } from "react";
+import { useState, useCallback, ChangeEvent } from "react";
 
-// useInputs라는 custom hook을 생성합니다. initialForm이라는 parameter를 받습니다.
-const useInputs = (initialForm: any) => {
-  // useState를 사용하여 form이라는 state와 setForm이라는 state를 생성합니다. initialForm을 초기값으로 설정합니다.
-  const [form, setForm] = useState(initialForm);
+interface Form {
+  [key: string]: string;
+}
 
-  // useCallback을 사용하여 onChange 함수를 생성합니다.
+type UseInputsReturn = [
+  Form,
+  (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  () => void
+];
+
+const useInputs = (initialForm: Form): UseInputsReturn => {
+  const [form, setForm] = useState<Form>(initialForm);
+
   const onChange = useCallback(
-    (e:any) => {
-      // e.target에서 name과 value를 추출합니다.
+    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       const { name, value } = e.target;
-      // setForm을 사용하여 form state를 업데이트합니다. 이때, 기존의 form state를 spread operator로 복사하고, name과 value를 업데이트합니다.
-      setForm((form:any) => ({ ...form, [name]: value }));
+      setForm((form) => ({ ...form, [name]: value }));
     },
     []
   );
 
-  // useCallback을 사용하여 reset 함수를 생성합니다. 이때, setForm을 사용하여 form state를 initialForm으로 초기화합니다.
   const reset = useCallback(() => setForm(initialForm), [initialForm]);
-  // form, onChange, reset을 반환합니다.
+
   return [form, onChange, reset];
 };
 
