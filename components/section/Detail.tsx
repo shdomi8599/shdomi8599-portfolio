1ee1e2c40df3e5624be5a2ee339e2d05462e09fd
shdomi8599 <shdomi@naver.com>
2023-04-06 13:24:25 +0900

    feat : Detail 섹션, 기능 구현 코드 자세히 보기

---
+++ components/section/Detail.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import Image from "next/image";
+import styled from "styled-components";
+import { useSetRecoilState } from "recoil";
+import { detailState } from "@/recoil/atom";
+
+const DetailBox = styled.article`
+  z-index: 99;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  > div {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    .back-btn {
+      position: absolute;
+      left: 100px;
+      top: 150px;
+      font-size: 1.4rem;
+      background-color: #d63636;
+      color: white;
+      padding: 20px;
+      border-radius: 20px;
+      cursor: pointer;
+      /* animation: motion 1s linear 0s infinite alternate;
+      margin-top: 0;
+      @keyframes motion {
+        0% {
+          margin-top: 0px;
+        }
+        100% {
+          margin-top: 50px;
+        }
+      } */
+    }
+  }
+
+  .loading {
+    background-color: white;
+    font-size: 2.5rem;
+    img {
+      margin-right: 10px;
+    }
+  }
+`;
+
+const Detail = () => {
+  //디테일 페이지 상태
+  const setDetail = useSetRecoilState(detailState);
+
+  /**
+   * 디테일 페이지 off이벤트
+   */
+  const offDetail = () => {
+    setDetail(false);
+  };
+  //로딩 상태
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  /**
+   * 페이지가 로드되면 로딩을 없애주기 위한 함수
+   */
+  const handleOnLoad = () => {
+    setIsLoaded(true);
+  };
+
+  return (
+    <DetailBox>
+      <div>
+        <div className="back-btn" onClick={offDetail}>
+          포트폴리오로 돌아가기
+        </div>
+        <iframe
+          src="https://web-beginner.tistory.com/"
+          width="100%"
+          height="100%"
+          allowFullScreen
+          onLoad={handleOnLoad}
+        />
+      </div>
+      {!isLoaded && (
+        <div className="loading">
+          <Image src="/preview.gif" alt="loading" width={40} height={40} />
+          로딩중입니다.
+        </div>
+      )}
+    </DetailBox>
+  );
+};
+
+export default Detail;
