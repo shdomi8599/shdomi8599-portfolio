76c3ec38c1189926c06aa3c3595322aa52c6103c
shdomi8599 <shdomi@naver.com>
2023-05-02 18:36:25 +0900

    style : modal opactity 조정

--- components/section/Skills.tsx
+++ components/section/Skills.tsx
@@ -137,9 +137,9 @@ const Skills = () => {
     ["Backend", BACK, moveBackend],
     ["Etc", ETC, moveEtc],
   ];
-  useEffect(() => {
-    setFrontend(true);
-  }, []);
+  // useEffect(() => {
+  //   setFrontend(true);
+  // }, []);
   return (
     <ArticleBox name="Skills">
       <Container>
@@ -213,7 +213,7 @@ const MainBox = styled.div<MainBoxProps>`
   position: relative;
   transition: 1.2s;
   height: ${(props) =>
-    props.frontend || props.backend || props.etc ? "950px" : "280px"};
+    props.frontend || props.etc ? "950px" : props.backend ? "600px" : "280px"};
   .CircularProgressbar .CircularProgressbar-text {
     fill: #636363;
     font-size: 1rem;
@@ -222,11 +222,12 @@ const MainBox = styled.div<MainBoxProps>`
     stroke: #5b6a70;
   }
   .card {
-    display: ${(props) => (props.modal ? "" : "none")};
     position: absolute;
     width: 250px;
-    height: 180px;
+    height: 200px;
+    opacity: ${(props) => (props.modal ? "1" : "0")};
     background-color: white;
+    transition: 0.3s;
     z-index: 3;
     box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
       0 2px 8px hsla(0, 0%, 0%, 0.05);
@@ -358,7 +359,7 @@ const MainBox = styled.div<MainBoxProps>`
     transition: 1.2s;
     position: absolute;
     right: ${(props) => (props.backend ? "477px" : "477px")};
-    top: ${(props) => (props.backend ? "500px" : "50px")};
+    top: ${(props) => (props.backend ? "300px" : "50px")};
     display: flex;
     justify-content: center;
     align-items: center;
